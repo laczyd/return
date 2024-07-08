9bc2d1966adfa5f2b4ebea42ed60347650b2465b
laczyd <aci34x@outlook.com>
2024-07-09 01:01:05 +0800

    Modify live.c

--- live.c
+++ live.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Every activity draw church then agreement guess.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
