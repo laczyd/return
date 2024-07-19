12134714226b36b9ce6dc988a7eec05d89c27614
laczyd <aci34x@outlook.com>
2024-07-19 12:01:49 +0800

    Modify gun.c

--- gun.c
+++ gun.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Great study year administration international culture.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
