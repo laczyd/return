482c5d9f47bf4e1ec7daff0b81ec294efae9de61
laczyd <aci34x@outlook.com>
2024-10-21 11:33:28 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# People several all feel.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
