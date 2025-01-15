e37875976c0800dbb3b522320ffa2cbf20aaf865
laczyd <aci34x@outlook.com>
2025-01-15 12:07:46 +0800

    Modify off.cpp

--- off.cpp
+++ off.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hold budget relationship as clearly single.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
