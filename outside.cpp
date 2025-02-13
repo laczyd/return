f3c32010a47533aff0ebe40ec94e4c6f9295da5e
laczyd <aci34x@outlook.com>
2025-02-13 15:03:18 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# After also between need law change.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
